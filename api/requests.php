6e0850fe6355f512383b9e16b189faee7adb4c2a
codewithdripzy <codewithdripzy@gmail.com>
2022-10-26 15:24:21 +0100

    made changes

--- api/requests.php
+++ api/requests.php
@@ -108,7 +108,41 @@
                 print_r(json_encode($flags));
                 return $flags;
             }
-            
+            else if($_REQUEST['request_type'] == 'deposit'){
+                $transaction->from_wallet_address = $_REQUEST['from_wallet_address'];
+                $transaction->to_wallet_address = $_REQUEST['to_wallet_address'];
+                $transaction->previous_hash = $transaction->getPreviousHash();
+                $transaction->current_hash = HashGen::GenerateHash()
+
+
+                if(isset($_REQUEST['transaction_type']) && !empty($_REQUEST['transaction_type'])){
+                    if(isset($_REQUEST['token']) && !empty($_REQUEST['token'])){
+                        
+                        if($wallet->deductAmount($transaction->to_wallet_address, $transaction->amount)){
+                            if($transaction->deleteTransaction($_REQUEST['token'], $_REQUEST['wallet_address'], $_REQUEST['transaction_type'])){
+                                $flags['state'] = true;
+                                $flags['msg'] = 'success';
+                            }else{
+                                $flags['state'] = false;
+                                $flags['msg'] = 'Something went wrong! it is not your fault.';
+                            }
+                        }else{
+                            $flags['msg'] = $transaction->to_wallet_address;
+                            $flags['state'] = false;
+                            $flags['msg'] = 'Something went wrong while deleting this record!';
+                        }
+                    }else{
+                        $flags['state'] = false;
+                        $flags['msg'] = 'Something went wrong! Invalid Request';
+                    }
+                }else{
+                    $flags['state'] = false;
+                    $flags['msg'] = 'Something went wrong! Invalid Request';
+                }  
+
+                print_r(json_encode($flags));
+                return $flags;
+            }
         }
     }
 ?>
